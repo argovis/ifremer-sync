78b0d29da615c52d2e5e3a06167f96d0665d6c20
billmills <you@example.com>
2022-08-20 00:44:05 -0400

    draft of nightly redo scripts

---
+++ ifremer-sync-redo.sh
@@ -0,0 +1,6 @@
+# redo the nightly update in the folder $1; assumes the rsync and `updatedprofiles` generation completed successfully, just redoes mongo load
+date=$(date '+%Y%m%dT%H%M%S')
+logdir=$1
+
+# redo load profiles, with logging
+while read i ; do python translateProfile.py $i >> ${logdir}/updatedprofiles.redo-${date}.log 2>&1 ; done < ${logdir}/updatedprofiles
